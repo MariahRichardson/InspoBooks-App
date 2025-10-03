94dfe1907e1b6b20ed1166caee650e0b3e00095d
yeawon1231 <yeawon1231@gmail.com>
2025-10-03 13:20:27 -0400

    Reverted namespace back to com.zybooks.inspobook

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.zybooks.inspobookapp"
+    namespace = "com.zybooks.inspobook"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.zybooks.inspobookapp"
+        applicationId = "com.zybooks.inspobook"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
