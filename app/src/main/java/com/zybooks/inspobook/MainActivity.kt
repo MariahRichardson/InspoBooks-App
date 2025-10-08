460de9d52a9d228f914bbcfdb5a3b944be4b2d6a
Floaty <90103866+FloatingStory@users.noreply.github.com>
2025-10-07 20:56:51 -0400

    Adjust bottom nav controller to show for only specific fragments and adjust inspobooks fragment ui

--- app/src/main/java/com/zybooks/inspobook/MainActivity.kt
+++ app/src/main/java/com/zybooks/inspobook/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zybooks.inspobook
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -22,7 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         bottomNavView = findViewById(R.id.bottomNavigationView)
         //set inspobooks page as starting page after login
-        //replaceFragment(InspoBooksFragment())
         bottomNavView.selectedItemId = R.id.mybooks
 
         //set up nav graph with the bottom navigation
@@ -30,18 +30,27 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         bottomNavView.setupWithNavController(navController)
 
+        //nav only appears for certain fragments, otherwise don't
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when(destination.id){
+                R.id.InspoBooksFragment,
+                R.id.UserProfileFragment -> {
+                    bottomNavView.visibility = View.VISIBLE
+                }
+                else ->  bottomNavView.visibility = View.GONE
+            }
+        }
+
         bottomNavView.setOnItemSelectedListener{item ->
             //when is the switch statement in Kotlin
             when(item.itemId){
                 R.id.profile -> {
-                    Log.d(TAG, "profile in NavBar clicked")
+                    //Log.d(TAG, "profile in NavBar clicked")
                     navController.navigate(R.id.UserProfileFragment)
-                    //replaceFragment(UserProfileFragment())
                     true
                 }
                 R.id.mybooks -> {
-                    Log.d(TAG, "my books in NavBar clicked")
-                    //replaceFragment(InspoBooksFragment())
+                    //Log.d(TAG, "my books in NavBar clicked")
                     navController.navigate(R.id.InspoBooksFragment)
                     true
                 }
@@ -71,7 +80,7 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
-    //replace main activity layout with fragment
+    //replace main activity layout with fragment, not used as of now
     private fun replaceFragment(fragment: Fragment){
         val fm = supportFragmentManager
         fm.beginTransaction()
