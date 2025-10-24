c453c60b7ac586db9b88fa750cb11be2649e627c
Floaty <90103866+FloatingStory@users.noreply.github.com>
2025-10-24 10:09:38 -0400

    Add InspoPagesViewModel

--- app/src/main/java/com/zybooks/inspobook/model/InspoPage.kt
+++ app/src/main/java/com/zybooks/inspobook/model/InspoPage.kt
@@ -5,32 +5,35 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.icu.number.IntegerWidth
 import androidx.core.graphics.createBitmap
+import java.io.File
 
-class InspoPage(inspobookID: String, canvasWidth: Int?, canvasHeight: Int?)
+class InspoPage()//inspobookID: String, canvasWidth: Int?, canvasHeight: Int?)
 {
+    //store id and content
     var pageID: String? = "dummy"
+    var contentOfPage: File = File("dummy")
 
-    //use bitmap to eventually compress and store as a file
-    var bitmapOfPage: Bitmap
-
-    //use canvas to actually do changes on
-    var canvasPage: Canvas
-
-    init {
-        //if user provided page width and height is not null and greater than or equal to 10, then initialize using given width and height
-        //TODO:OR REMOVE AND MAKE IT A SET SIZE
-        if(canvasWidth != null && canvasWidth >= 10 && canvasHeight != null && canvasHeight >= 10) {
-            bitmapOfPage = createBitmap(canvasWidth, canvasHeight)
-        }
-        else{
-            //else create a canvas of size 10 x 10
-            bitmapOfPage = createBitmap(10,10)
-        }
-
-        //default background of created page to white
-        bitmapOfPage.eraseColor(Color.WHITE)
-
-        //make a Canvas with the bitmap created
-        canvasPage = Canvas(bitmapOfPage)
-    }
+//    //use bitmap to eventually compress and store as a file
+//    var bitmapOfPage: Bitmap
+//
+//    //use canvas to actually do changes on
+//    var canvasPage: Canvas
+//
+//    init {
+//        //if user provided page width and height is not null and greater than or equal to 10, then initialize using given width and height
+//        //TODO:OR REMOVE AND MAKE IT A SET SIZE
+//        if(canvasWidth != null && canvasWidth >= 10 && canvasHeight != null && canvasHeight >= 10) {
+//            bitmapOfPage = createBitmap(canvasWidth, canvasHeight)
+//        }
+//        else{
+//            //else create a canvas of size 10 x 10
+//            bitmapOfPage = createBitmap(10,10)
+//        }
+//
+//        //default background of created page to white
+//        bitmapOfPage.eraseColor(Color.WHITE)
+//
+//        //make a Canvas with the bitmap created
+//        canvasPage = Canvas(bitmapOfPage)
+//    }
 }
---
+++ app/src/main/java/com/zybooks/inspobook/viewmodel/InspoPagesViewModel.kt
@@ -0,0 +1,44 @@
+package com.zybooks.inspobook.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.zybooks.inspobook.model.InspoBook
+import com.zybooks.inspobook.model.InspoPage
+import kotlin.collections.orEmpty
+import kotlin.collections.toMutableList
+
+class InspoPagesViewModel(bookPages: List<InspoPage>): ViewModel() {
+
+    //list of pages to navigate to within a book
+    private var pages: MutableLiveData<MutableList<InspoPage>> = MutableLiveData<MutableList<InspoPage>>(ArrayList())
+    val pageList: LiveData<MutableList<InspoPage>> get() = pages
+
+    //page being displayed to user and can be edited
+    private lateinit var currentPage: MutableLiveData<InspoPage>
+    val displayedPage: LiveData<InspoPage> get() = currentPage
+
+    //set an InspoBook object and get its list of pages to display
+    fun setSelectedBook(book: InspoBook){
+        if(book.listOfPages.isNotEmpty()) {
+            pages.value = book.listOfPages.orEmpty().toMutableList()
+        }
+        else{
+            //if book has no pages, add one default page
+            addPage()
+        }
+    }
+
+    fun addPage(){
+        var newIPage = InspoPage()
+        val updatedPageList = pages.value.orEmpty().toMutableList()
+        updatedPageList.add(newIPage)
+
+        //assignment will trigger MutableLiveData update
+        pages.value = updatedPageList
+    }
+
+    fun removePage(){
+
+    }
+}
